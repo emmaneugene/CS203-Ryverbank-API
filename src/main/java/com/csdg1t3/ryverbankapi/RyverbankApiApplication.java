bef9ccaff6f6a953b91e1a961a80ebfbb48af880
Emmanuel Oh <emmaneugene@outlook.com>
2020-09-05 22:04:40 +0800

    first commit, create project with spring initializr

---
+++ src/main/java/com/csdg1t3/ryverbankapi/RyverbankApiApplication.java
@@ -0,0 +1,13 @@
+package com.csdg1t3.ryverbankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyverbankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyverbankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csdg1t3/ryverbankapi/RyverbankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csdg1t3.ryverbankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyverbankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
