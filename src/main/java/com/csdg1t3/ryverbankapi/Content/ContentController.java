aacae75668ad19532390387736687811486e049b
Nikki Poo <nikki.poo.2019@smu.edu.sg>
2020-09-24 15:30:48 +0800

    fixed bugs

--- src/main/java/com/csdg1t3/ryverbankapi/Content/ContentController.java
+++ src/main/java/com/csdg1t3/ryverbankapi/Content/ContentController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import jdk.javadoc.internal.doclets.formats.html.Contents;
-
 @RestController
 public class ContentController {
     private ContentService contentService;
