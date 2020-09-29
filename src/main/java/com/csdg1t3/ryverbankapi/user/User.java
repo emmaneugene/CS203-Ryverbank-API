7b1b1e3c76a708765cab902cb4432e8b88234f5f
Nikki Poo <nikki.poo.2019@smu.edu.sg>
2020-09-29 15:58:00 +0800

    add user classes

---
+++ src/main/java/com/csdg1t3/ryverbankapi/user/User.java
@@ -0,0 +1,51 @@
+package com.csdg1t3.ryverbankapi.user;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.*;
+
+public class User implements UserDetails{
+    private static final long serialVersionUID = 1L;
+
+    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+    
+    @NotNull(message = "Username should not be null")
+    @Size(min = 5, max = 20, message = "Username should be between 5 and 20 characters")
+    private String username;
+    
+    @NotNull(message = "Password should not be null")
+    @Size(min = 8, message = "Password should be at least 8 characters")
+    private String password;
+
+    @NotNull(message = "Authorities should not be null")
+    // We define two roles/authorities: ROLE_USER or ROLE_ADMIN
+    private String authorities;
+
+    public User(String username, String password, String authorities){
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+
+    /* Return a collection of authorities (roles) granted to the user.
+    */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority(authorities));
+    }
+
+    
+}
