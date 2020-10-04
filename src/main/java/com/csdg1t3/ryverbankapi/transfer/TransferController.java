14603f1ea8a922a08abb357b429514b2ecdf9474
Carmen Yip <carmen.yip.2019@smu.edu.sg>
2020-10-04 23:28:04 +0800

    edit transferController

--- src/main/java/com/csdg1t3/ryverbankapi/transfer/TransferController.java
+++ src/main/java/com/csdg1t3/ryverbankapi/transfer/TransferController.java
@@ -54,7 +54,7 @@ public class TransferController {
         // Handle "transfer not found" error using appropriate http codes
         if (transfer == null) throw new TransferNotFoundException(transferId);
 
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Authentication auth = Security.getContext().getAuthentication();
         String username = auth.getPrincipal().toString().substring(0, auth.getPrincipal().toString().indexOf(" "));
         // System.out.println(username);
         // System.out.println(transfer.getReceiver().getCustomer().getUsername());
