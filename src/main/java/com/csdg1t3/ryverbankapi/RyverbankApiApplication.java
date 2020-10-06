64894effe3fa9abb1567867e139045c175020231
Emmanuel Oh <emmaneugene@outlook.com>
2020-10-06 16:06:22 +0800

    update gitignore and main application

--- src/main/java/com/csdg1t3/ryverbankapi/RyverbankApiApplication.java
+++ src/main/java/com/csdg1t3/ryverbankapi/RyverbankApiApplication.java
@@ -4,9 +4,6 @@ import java.util.*;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
