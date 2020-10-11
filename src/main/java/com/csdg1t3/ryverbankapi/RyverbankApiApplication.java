cf4330fd6616de8ae6d9ab6e858b81dc813b4e1a
Emmanuel Oh <emmaneugene@outlook.com>
2020-10-11 15:30:16 +0800

    create analyst accounts at application startup

--- src/main/java/com/csdg1t3/ryverbankapi/RyverbankApiApplication.java
+++ src/main/java/com/csdg1t3/ryverbankapi/RyverbankApiApplication.java
@@ -22,10 +22,14 @@ public class RyverbankApiApplication {
         User admin = new User(1, "admin", "S1234567G", "80756529", "Lalaland 10, Potato's Dream, 10200", "manager_1", encoder.encode("01_manager_01"), "ROLE_MANAGER", true);
         User tstark = new User(2, "Tony Stark", "S8732269I", "80437586", "10880 Malibu Point, 90265", "iamironman", encoder.encode("i<3carmen"), "ROLE_USER", true);
         User tholland = new User(3, "Tom Holland", "S9847385E", "93580378", "20 Ingram Street", "spiderman", encoder.encode("mrstark,Idontfeels0good"), "ROLE_USER", true);
+        User analyst1 = new User(4, "analyst 1", "S1234567D", "00000000", "nil", "analyst_1", encoder.encode("01_analyst_01"), "ROLE_ANALYST", true);
+        User analyst2 = new User(5, "analyst 2", "S1234567D", "00000000", "nil", "analyst_2", encoder.encode("02_analyst_02"), "ROLE_ANALYST", true);
 
         System.out.println("[Add manager]: " + users.save(admin));
         System.out.println("[Add customer]: " + users.save(tstark));
         System.out.println("[Add customer]: " + users.save(tholland));
+        System.out.println("[Add analyst]: " + users.save(analyst1));
+        System.out.println("[Add analyst]: " + users.save(analyst2));
 
         // RestTemplateClient client = ctx.getBean(RestTemplateClient.class);
         // User cx = new User(4, "Carmmie Yip", "S9984627E", 93749560, "66 Lorong 4 Toa Payoh #01-317 S310066", "potatoes", encoder.encode("p0tatoes<3"),  "MANAGER", true);
