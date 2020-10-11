8e654bc6e368a423c8eb92ba20600c1e3b15be36
Emmanuel Oh <emmaneugene@outlook.com>
2020-10-11 17:25:44 +0800

    resolve errors with analyst data

--- src/main/java/com/csdg1t3/ryverbankapi/RyverbankApiApplication.java
+++ src/main/java/com/csdg1t3/ryverbankapi/RyverbankApiApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.csdg1t3.ryverbankapi.client.RestTemplateClient;
+import com.csdg1t3.ryverbankapi.content.*;
 import com.csdg1t3.ryverbankapi.user.*;
 import com.csdg1t3.ryverbankapi.security.*;
 
@@ -17,13 +18,14 @@ public class RyverbankApiApplication {
         ApplicationContext ctx = SpringApplication.run(RyverbankApiApplication.class, args);
 
         UserRepository users = ctx.getBean(UserRepository.class);
+        ContentRepository contents = ctx.getBean(ContentRepository.class);
         BCryptPasswordEncoder encoder = ctx.getBean(BCryptPasswordEncoder.class);
 
         User admin = new User(1, "admin", "S1234567G", "80756529", "Lalaland 10, Potato's Dream, 10200", "manager_1", encoder.encode("01_manager_01"), "ROLE_MANAGER", true);
         User tstark = new User(2, "Tony Stark", "S8732269I", "80437586", "10880 Malibu Point, 90265", "iamironman", encoder.encode("i<3carmen"), "ROLE_USER", true);
         User tholland = new User(3, "Tom Holland", "S9847385E", "93580378", "20 Ingram Street", "spiderman", encoder.encode("mrstark,Idontfeels0good"), "ROLE_USER", true);
-        User analyst1 = new User(4, "analyst 1", "S1234567D", "00000000", "nil", "analyst_1", encoder.encode("01_analyst_01"), "ROLE_ANALYST", true);
-        User analyst2 = new User(5, "analyst 2", "S1234567D", "00000000", "nil", "analyst_2", encoder.encode("02_analyst_02"), "ROLE_ANALYST", true);
+        User analyst1 = new User(4, "analyst 1", "S1234567D", "00000000", "no address", "analyst_1", encoder.encode("01_analyst_01"), "ROLE_ANALYST", true);
+        User analyst2 = new User(5, "analyst 2", "S1234567D", "00000000", "no address", "analyst_2", encoder.encode("02_analyst_02"), "ROLE_ANALYST", true);
 
         System.out.println("[Add manager]: " + users.save(admin));
         System.out.println("[Add customer]: " + users.save(tstark));
@@ -31,6 +33,8 @@ public class RyverbankApiApplication {
         System.out.println("[Add analyst]: " + users.save(analyst1));
         System.out.println("[Add analyst]: " + users.save(analyst2));
 
+        contents.save(new Content(1, "Title1", "Summary1", "Content1", "Link1", true));
+        contents.save(new Content(2, "Title2", "Summary2", "Content2", "Link2", false));
         // RestTemplateClient client = ctx.getBean(RestTemplateClient.class);
         // User cx = new User(4, "Carmmie Yip", "S9984627E", 93749560, "66 Lorong 4 Toa Payoh #01-317 S310066", "potatoes", encoder.encode("p0tatoes<3"),  "MANAGER", true);
         // System.out.println("[Add customer, manager]: " + client.addUser("http://localhost:8080/customers", cx));
