c7d98e86b30a2ef8620720b8477c787a9354e704
Emmanuel Oh <emmaneugene@outlook.com>
2020-10-25 18:35:38 +0800

    add TransferService depenedency and edit methods in StockController

--- src/main/java/com/csdg1t3/ryverbankapi/trade/StockController.java
+++ src/main/java/com/csdg1t3/ryverbankapi/trade/StockController.java
@@ -1,14 +1,5 @@
 package com.csdg1t3.ryverbankapi.trade;
 
-import java.util.Scanner;
-import java.util.Optional;
-import java.util.List;
-import java.util.Random;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,21 +16,28 @@ import org.springframework.security.core.Authentication;
 
 import javax.validation.Valid;
 
+import java.util.*;
+import java.io.*;
+
 @RestController
 public class StockController {
     private StockRepository stockRepo;
     private TradeRepository tradeRepo;
+    private TradeService tradeService;
 
-    public StockController(StockRepository stockRepo, TradeRepository tradeRepo){
+    public StockController(StockRepository stockRepo, TradeRepository tradeRepo, 
+    TradeService tradeService){
         this.stockRepo = stockRepo;
         this.tradeRepo = tradeRepo;
+        this.tradeService = tradeService;
     }
 
     /**  
      * Market maker function that creates stocks as well as market maker buy and sell bids 
      * given a list of stock symbols.
      * 
-     * This method should be called by the main method during application startup
+     * This method should be called by the main method during application startup, with a file
+     * containing a list stock symbols and last prices provided
      */
     public void createStocks(File data){
         Random random = new Random();
@@ -60,7 +58,7 @@ public class StockController {
                 marketBuy.setQuantity(volume);
                 marketBuy.setBid(bid);
                 marketBuy.setAvg_price(0);
-                marketBuy.setDate(System.currentTimeMillis()/1000);
+                marketBuy.setDate(System.currentTimeMillis());
                 marketBuy.setAccount_id(Long.valueOf(0));
                 marketBuy.setCustomer_id(Long.valueOf(0));
                 marketBuy.setStatus("open");
@@ -71,7 +69,7 @@ public class StockController {
                 marketSell.setQuantity(volume);
                 marketSell.setAsk(ask);
                 marketSell.setAvg_price(0);
-                marketSell.setDate(System.currentTimeMillis()/1000);
+                marketSell.setDate(System.currentTimeMillis());
                 marketSell.setAccount_id(Long.valueOf(0));
                 marketSell.setCustomer_id(Long.valueOf(0));
                 marketSell.setStatus("open");
@@ -88,27 +86,71 @@ public class StockController {
     }
 
     /**
-     * Get all publicly traded stocks. Controller will call TradeService to get the most recent 
-     * information on
+     * Get all publicly traded stocks. This method calls updateStockDetails() 
+     * to get the most recent information on ask, ask volume, bid and bid volume. 
+     * 
+     * @return list of all tradable stocks
      */
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/stocks")
     public List<Stock> getStocks() {
-        return stockRepo.findAll();
+        List<Stock> stocks = stockRepo.findAll();
+
+        for (Stock stock : stocks) {
+            updateStockDetails(stock);
+        }
+
+        return stocks;
     }
 
     /**
-     * Get individual stock
+     * Get an individual stock as specified by its symbol. This method calls updateStockDetails() 
+     * to get the most recent information on bid volume, bid, ask volume and ask
+     * 
+     * @return the stock specified by the symbol
      */
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/stocks/{symbol}")
     public Stock getStock(@PathVariable String symbol) {
-        Optional<Stock> result = stockRepo.findBySymbol(symbol);
-        
-        if (!result.isPresent())
+        if (!stockRepo.existsBySymbol(symbol))
             throw new StockNotFoundException(symbol);
+        
+        Stock stock = stockRepo.findBySymbol(symbol).get();
+        updateStockDetails(stock);
 
-        return result.get();
+        return stock;
+    }
+
+    /**
+     * Updates a stock's bid volume, bid, ask volume and ask using methods from
+     * TradeService. It then saves the updated stock to the database
+     * 
+     * @param stock
+     */
+    public void updateStockDetails(Stock stock) {
+        double bid, ask;
+        bid = ask = 0;
+        int bid_volume, ask_volume;
+        bid_volume = ask_volume = 0;
+        Trade buy = tradeService.getHighestBidTradeForStock(stock.getSymbol());
+        Trade sell = tradeService.getLowestAskTradeForStock(stock.getSymbol());
+        
+        if (buy != null) {
+            bid_volume = buy.getRemaining_quantity();
+            bid = buy.getBid();
+        }
+
+        if (sell != null) {
+            ask_volume = sell.getRemaining_quantity();
+            ask = sell.getAsk();
+        }
+
+        stock.setBid_volume(bid_volume);
+        stock.setBid(bid);
+        stock.setAsk_volume(ask_volume);
+        stock.setAsk(ask);
+
+        stockRepo.save(stock);
     }
 }
 
