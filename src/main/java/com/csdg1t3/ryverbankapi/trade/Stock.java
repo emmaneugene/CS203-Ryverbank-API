5f138934507e758ca6bf612a8d77f554107f0110
Emmanuel Oh <emmaneugene@outlook.com>
2020-10-30 12:39:39 +0800

    refactor trade classes, remove TradeService interface, rename TradeServiceImpl

--- src/main/java/com/csdg1t3/ryverbankapi/trade/Stock.java
+++ src/main/java/com/csdg1t3/ryverbankapi/trade/Stock.java
@@ -1,11 +1,6 @@
 package com.csdg1t3.ryverbankapi.trade;
 
 import javax.persistence.*;
-import javax.validation.constraints.*;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import javax.validation.Valid;
-
-import java.util.*;
 
 /**
  * POJO that stores the details of a stock
@@ -15,19 +10,14 @@ public class Stock {
     @Id
     private String symbol;
     
-    @NotNull(message = "last price should not be null")
     private double last_price;
 
-    @NotNull(message = "bid volume should not be null")
     private int bid_volume;
 
-    @NotNull(message = "bid should not be null")
     private double bid;
 
-    @NotNull(message = "ask_volume should not be null")
     private int ask_volume;
 
-    @NotNull(message = "ask should not be null")
     private double ask; 
 
     /**
@@ -53,57 +43,34 @@ public class Stock {
         this.ask = ask;
     }
 
-    public String getSymbol(){
-        return symbol;
-    }
+    public String getSymbol() { return symbol; }
 
-    public double getLast_price(){
-        return last_price;
-    }
+    public double getLast_price() { return last_price; }
 
-    public int getBid_volume(){
-        return bid_volume;
-    }
+    public int getBid_volume() { return bid_volume; }
 
-    public double getBid(){
-        return bid;
-    }
+    public double getBid() { return bid; }
 
-    public int getAsk_volume(){
-        return ask_volume;
-    }
+    public int getAsk_volume() { return ask_volume; }
 
-    public double getAsk(){
-        return ask;
-    }
+    public double getAsk(){ return ask; }
 
-    public void setSymbol(String symbol){
-        this.symbol = symbol;
-    }
+    public void setSymbol(String symbol) { this.symbol = symbol; }
 
-    public void setLast_price(double last_price){
-        this.last_price = last_price;
-    }
+    public void setLast_price(double last_price) { this.last_price = last_price; }
 
-    public void setBid_volume(int bid_volume){
-        this.bid_volume = bid_volume;
-    }
+    public void setBid_volume(int bid_volume) { this.bid_volume = bid_volume; }
 
-    public void setBid(double bid){
-        this.bid = bid;
-    }
+    public void setBid(double bid) { this.bid = bid; }
 
-    public void setAsk_volume(int ask_volume){
-        this.ask_volume = ask_volume;
-    }
+    public void setAsk_volume(int ask_volume) { this.ask_volume = ask_volume; }
 
-    public void setAsk(double ask){
-        this.ask = ask;
-    }
+    public void setAsk(double ask) { this.ask = ask; }
 
     @Override
     public String toString() {
-        return String.format("Symbol[id=%s, Last Price=%f, Bid Volume=%d, Bid = %f, Ask Volume = %d, Ask=%f]", symbol, last_price, bid_volume, bid, ask_volume, ask);
+        return String.format("Symbol[id=%s, Last Price=%.2f, Bid Volume=%d, Bid = %.2f, Ask Volume = %d, Ask=%.2f]", 
+        symbol, last_price, bid_volume, bid, ask_volume, ask);
     }
 
 }
