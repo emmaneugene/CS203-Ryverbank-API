30442a8e22bc51928b39f1fe947f883c49ff1f8c
Sin Yee Kwek <sinyeekwek@gmail.com>
2020-11-02 21:54:30 +0800

    added test classes for ContentControllerTest,TradeControllerTest and TradeServiceTest

--- src/test/java/com/csdg1t3/ryverbankapi/UserControllerTest.java
+++ src/test/java/com/csdg1t3/ryverbankapi/UserControllerTest.java
@@ -10,7 +10,11 @@ import static org.mockito.Mockito.when;
 
 import java.util.*;
 
+import com.csdg1t3.ryverbankapi.trade.*;
 import com.csdg1t3.ryverbankapi.user.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.csdg1t3.ryverbankapi.security.*;
+
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,10 +22,21 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import jdk.jfr.Timestamp;
+
 @ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
     @Mock
     private UserRepository userRepo;
+
+    @Mock 
+    private PortfolioRepository portfolioRepo;
+
+    @Mock
+    private UserAuthenticator uAuth;
+
+    @Mock
+    private BCryptPasswordEncoder encoder;
 
     @InjectMocks
     private UserController userController;
@@ -29,7 +44,7 @@ public class UserControllerTest {
     // Assert that method returns saved user
     @Test
     void addCustomer_NewUserName_ReturnSavedCustomer() {
-
+        // mock 
     }
 
     // Assert that method throws UserNotValidException
@@ -38,4 +53,44 @@ public class UserControllerTest {
 
     }
 
+    @Test
+    void getCustomers_NoCustomers_returnNullList(){
+        // List<User>
+        when(userRepo.findAll()).thenReturn(null);
+    }
+
+    @Test
+    void getCustomers_notUnauthorised_returnForbidden(){
+
+    }
+
+    @Test
+    void getCustomers_WithCustomers_returnListOfCustomers(){
+
+    }
+
+    @Test
+    void getCustomer_notInRepository_throwUserNotFoundException(){
+
+    }
+
+    @Test
+    void getCustomer_notAuthenticated_returnForbbidden(){
+
+    }
+
+    @Test
+    void getCustomer_notAuthorised_returnForbbidden(){
+
+    }
+
+    @Test
+    void getCustomer_AuthenticatedAndFound_returnUser(){
+
+    }
+
+    @Test
+    void getCustomer_AuthenticatedAndNotFound_throwUserNotFoundException(){
+
+    }
 }
