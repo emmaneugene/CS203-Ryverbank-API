95b4e9be5730bc73a76eecb996df52c1650ac14c
Sin Yee Kwek <sinyeekwek@gmail.com>
2020-11-03 12:36:06 +0800

    uncomment application

--- src/main/java/com/csdg1t3/ryverbankapi/RyverbankApiApplication.java
+++ src/main/java/com/csdg1t3/ryverbankapi/RyverbankApiApplication.java
@@ -46,29 +46,29 @@ public class RyverbankApiApplication {
         PortfolioRepository portfolios = ctx.getBean(PortfolioRepository.class);
         StockController stockController = ctx.getBean(StockController.class);
 
-        // User admin = new User(null, "admin", "S1234567G", "80756529", "Lalaland 10, Potato's Dream, 10200", "manager_1", encoder.encode("01_manager_01"), "ROLE_MANAGER", true);
-        // User tstark = new User(null, "Tony Stark", "S8732269I", "80437586", "10880 Malibu Point, 90265", "iamironman", encoder.encode("i<3carmen"), "ROLE_USER", true);
-        // User tholland = new User(null, "Tom Holland", "S9847385E", "93580378", "20 Ingram Street", "spiderman", encoder.encode("mrstark,Idontfeels0good"), "ROLE_USER", true);
-        // User analyst1 = new User(null, "analyst 1", "S1234567D", "00000000", "no address", "analyst_1", encoder.encode("01_analyst_01"), "ROLE_ANALYST", true);
-        // User analyst2 = new User(null, "analyst 2", "S1234567D", "00000000", "no address", "analyst_2", encoder.encode("02_analyst_02"), "ROLE_ANALYST", true);
+        User admin = new User(null, "admin", "S1234567G", "80756529", "Lalaland 10, Potato's Dream, 10200", "manager_1", encoder.encode("01_manager_01"), "ROLE_MANAGER", true);
+        User tstark = new User(null, "Tony Stark", "S8732269I", "80437586", "10880 Malibu Point, 90265", "iamironman", encoder.encode("i<3carmen"), "ROLE_USER", true);
+        User tholland = new User(null, "Tom Holland", "S9847385E", "93580378", "20 Ingram Street", "spiderman", encoder.encode("mrstark,Idontfeels0good"), "ROLE_USER", true);
+        User analyst1 = new User(null, "analyst 1", "S1234567D", "00000000", "no address", "analyst_1", encoder.encode("01_analyst_01"), "ROLE_ANALYST", true);
+        User analyst2 = new User(null, "analyst 2", "S1234567D", "00000000", "no address", "analyst_2", encoder.encode("02_analyst_02"), "ROLE_ANALYST", true);
 
         // Add users
-        // System.out.println("[Add manager]: " + users.save(admin));
-        // System.out.println("[Add customer]: " + users.save(tstark));
-        // System.out.println("[Add customer]: " + users.save(tholland));
-        // System.out.println("[Add analyst]: " + users.save(analyst1));
-        // System.out.println("[Add analyst]: " + users.save(analyst2));
+        System.out.println("[Add manager]: " + users.save(admin));
+        System.out.println("[Add customer]: " + users.save(tstark));
+        System.out.println("[Add customer]: " + users.save(tholland));
+        System.out.println("[Add analyst]: " + users.save(analyst1));
+        System.out.println("[Add analyst]: " + users.save(analyst2));
 
         // Add portfolios
-        // portfolios.save(new Portfolio(null, tstark.getId(), tstark, null, 0, 0));
-        // portfolios.save(new Portfolio(null, tholland.getId(), tholland, null, 0, 0));
+        portfolios.save(new Portfolio(null, tstark.getId(), tstark, null, 0, 0));
+        portfolios.save(new Portfolio(null, tholland.getId(), tholland, null, 0, 0));
         
         // Add content
-        // contents.save(new Content(null, "Title1", "Summary1", "Content1", "Link1", true));
-        // contents.save(new Content(null, "Title2", "Summary2", "Content2", "Link2", false));
+        contents.save(new Content(null, "Title1", "Summary1", "Content1", "Link1", true));
+        contents.save(new Content(null, "Title2", "Summary2", "Content2", "Link2", false));
         
         // Add stocks and market maker trades
-        // stockController.createStocks();
+        stockController.createStocks();
     }
 
     
