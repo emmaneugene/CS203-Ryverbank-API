6109c6135ef5c349f28067a5cbcdb779a21cfbed
Sin Yee Kwek <sinyeekwek@gmail.com>
2020-11-04 12:42:12 +0800

    edit security config

--- src/main/java/com/csdg1t3/ryverbankapi/security/SecurityConfig.java
+++ src/main/java/com/csdg1t3/ryverbankapi/security/SecurityConfig.java
@@ -49,7 +49,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers(HttpMethod.GET, "/customers/*").hasAnyRole("MANAGER", "USER")
             .antMatchers(HttpMethod.POST, "/customers").hasRole("MANAGER")
             .antMatchers(HttpMethod.PUT, "/customers/*").hasAnyRole("MANAGER", "USER")
-            .antMatchers(HttpMethod.DELETE).hasRole(" ")
             // accounts 
             .antMatchers(HttpMethod.GET, "/accounts/*").hasRole( "USER")
             .antMatchers(HttpMethod.POST, "/accounts/*/transactions").hasRole("USER")
