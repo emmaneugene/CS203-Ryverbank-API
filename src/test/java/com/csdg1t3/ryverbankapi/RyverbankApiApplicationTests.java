a87f12e349eb58869ae93c64fac175e6ee61acbf
Sin Yee Kwek <sinyeekwek@gmail.com>
2020-11-04 15:17:35 +0800

    prepare Integrated test

--- src/test/java/com/csdg1t3/ryverbankapi/RyverbankApiApplicationTests.java
+++ src/test/java/com/csdg1t3/ryverbankapi/RyverbankApiApplicationTests.java
@@ -1,10 +1,54 @@
 package com.csdg1t3.ryverbankapi;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootTest
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.URI;
+import java.util.Optional;
+
+import com.csdg1t3.ryverbankapi.*;
+import com.csdg1t3.ryverbankapi.user.UserRepository;
+
+@SpringBootTest(WebEnvironment = WebEnvironment.RANDOM_PORT)
 class RyverbankApiApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	private final String baseUrl = "http://localhost:";
+
+	@Autowired
+	/**
+	 * Use TestRestTemplate for testing a real instance of your application as an external actor.
+	 * TestRestTemplate is just a convenient subclass of RestTemplate that is suitable for integration tests.
+ 	 * It is fault tolerant, and optionally can carry Basic authentication headers.
+	 */
+	private TestRestTemplate restTemplate;
+
+	@Autowired
+	private UserRepository users;
+
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+
+	@AfterEach
+	void tearDown(){
+		users.deleteAll();
+	}
+
+	
 
 	@Test
 	void contextLoads() {
