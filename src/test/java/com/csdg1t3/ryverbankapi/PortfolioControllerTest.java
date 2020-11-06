67ff2eb59d7e977467981e04cb66719434c23686
Carmen Yip <carmen.yip.2019@smu.edu.sg>
2020-11-07 00:20:10 +0800

    Add test for PortfolioController

--- src/test/java/com/csdg1t3/ryverbankapi/PortfolioControllerTest.java
+++ src/test/java/com/csdg1t3/ryverbankapi/PortfolioControllerTest.java
@@ -39,4 +39,28 @@ public class PortfolioControllerTest {
 
     @InjectMocks
     PortfolioController portfolioController;
+
+    private final String u1_FULL_NAME = "cspotatoes";
+    private final String  u1_USERNAME = "potato";
+    private final String u1_PASSWORD = "iamgoodpotato123";
+    private final String u1_ROLE = "ROLE_USER";
+    private final String NRIC = "S1234567G";
+    private final String PHONE_NO = "93223235";
+
+    @Test
+    void getPortfolio_validUser_ReturnPortfolio() {
+        Long id = Long.valueOf(1);
+        User user = new User(id, u1_FULL_NAME, NRIC, PHONE_NO, "Test Address", u1_USERNAME, u1_PASSWORD, u1_ROLE, true);
+        List<Asset> assets = new ArrayList<Asset>();
+        Portfolio portfolio = new Portfolio(id, user.getId(), user, assets, 0.0, 0.0);
+        
+        when(uAuth.getAuthenticatedUser()).thenReturn(user);
+        when(portfolioRepo.findByCustomerId(any(Long.class))).thenReturn(Optional.of(portfolio));
+
+        Portfolio returnedPortfolio = portfolioController.getPortfolio();
+
+        assertEquals(returnedPortfolio, portfolio);
+        verify(uAuth).getAuthenticatedUser();
+        verify(portfolioRepo).findByCustomerId(id);
+    }
 }
