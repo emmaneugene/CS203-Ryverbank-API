e6b4aa48d78ea493b260db4af9542d75cb638bf8
Sin Yee Kwek <sinyeekwek@gmail.com>
2020-11-07 10:51:21 +0800

    add StockControllerTest

--- src/test/java/com/csdg1t3/ryverbankapi/StockControllerTest.java
+++ src/test/java/com/csdg1t3/ryverbankapi/StockControllerTest.java
@@ -28,4 +28,53 @@ public class StockControllerTest {
 
     @InjectMocks
     private StockController stockController;
+
+    private static final Stock stock1 = new Stock("A17U",3.23,20000, 3.19, 20000,3.24);
+    private static final Stock stock2 = new Stock("A17U",1.65,20000, 1.51, 20000,1.81);
+
+
+    @Test
+    void getStocks_NoStocks_ReturnEmptyList(){
+        List<Stock> stocks = new ArrayList<Stock>();
+
+        when(stockSvc.getAllUpdatedStocks()).thenReturn(stocks);
+
+        List<Stock> returned = stockController.getStocks();
+
+        assertEquals(returned,stocks);
+        verify(stockSvc).getAllUpdatedStocks();
+    }
+
+    @Test
+    void getStocks_AllStocks_ReturnAllStocks(){
+        List<Stock> stocks = new ArrayList<Stock>();
+        stocks.add(stock1);
+        stocks.add(stock2);
+
+        when(stockSvc.getAllUpdatedStocks()).thenReturn(stocks);
+
+        List<Stock> returned = stockController.getStocks();
+
+        assertEquals(returned,stocks);
+        verify(stockSvc).getAllUpdatedStocks();
+    }
+
+    @Test
+    void getStock_NoStock_ThrowStockNotFoundException(){
+        when(stockSvc.getUpdatedStock(any(String.class))).thenReturn(Optional.empty());
+
+        assertThrows(StockNotFoundException.class, () ->  stockController.getStock("XXXX"), "Could not find stock XXXX");
+        verify(stockSvc).getUpdatedStock("XXXX");
+
+    }
+
+    @Test
+    void getStock_FoundStock_ReturnStockWithUpdates(){
+        Optional<Stock> found = Optional.of(stock1);
+        when(stockSvc.getUpdatedStock(any(String.class))).thenReturn(found);
+
+        Stock returned = stockController.getStock("A17U");
+        assertEquals(stock1,returned);
+        verify(stockSvc).getUpdatedStock("A17U");
+    }
 }
